c4a9b81880059c0e335f2514a28fcfc3678f15d3
poohoh <kjh980328@gmail.com>
2023-02-07 20:36:27 +0900

    .

---
+++ src/ch10/No_03_25305.java
@@ -0,0 +1,14 @@
+package ch10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_03_25305 {
+    public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int num =
+    }
+}
