b65915f8cb3109b36c4d8b221d58b92011ef3f9b
poohoh <kjh980328@gmail.com>
2023-02-20 17:10:47 +0900

    ThisIsCodingTest ch03_greedy No_03

---
+++ src/thisIsCodingTest/ch03_greedy/No_03.java
@@ -0,0 +1,39 @@
+package thisIsCodingTest.ch03_greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No_03 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int min = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            Arrays.sort(arr[i]);
+
+            if (min < arr[i][0]) {
+                min = arr[i][0];
+            }
+        }
+
+        System.out.println(min);
+
+    }
+}
