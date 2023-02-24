70de4e285906e3c8cd227c3dc45696ba4a30871c
poohoh <kjh980328@gmail.com>
2023-02-24 15:37:46 +0900

    ThisIsCodingTest ch05_DFS_BFS No_03

---
+++ src/thisIsCodingTest/ch05_DFS_BFS/No_03.java
@@ -0,0 +1,12 @@
+package thisIsCodingTest.ch05_DFS_BFS;
+
+public class No_03 {
+    public static void main(String args[]) {
+        recursive();
+    }
+
+    public static void recursive() {
+        System.out.println("재귀 함수를 호출합니다.");
+        recursive();
+    }
+}
