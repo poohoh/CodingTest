41e6ae78204f6efe10cf6a69935f176fa93314a9
poohoh <kjh980328@gmail.com>
2023-02-25 21:59:05 +0900

    ThisIsCodingTest ch06_sort No_01

---
+++ src/thisIsCodingTest/ch06_sort/No_01.java
@@ -0,0 +1,32 @@
+package thisIsCodingTest.ch06_sort;
+
+public class No_01 {
+    public static void main(String args[]) {
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = arr[i];
+            int minIdx = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+
+            swap(arr, i, minIdx);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
