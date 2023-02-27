39ca111f727fefd37e2c0c3a48ddd735bbdbd12c
poohoh <kjh980328@gmail.com>
2023-02-27 11:20:11 +0900

    ThisIsCodingTest ch06_sort No_06 계수정렬

---
+++ src/thisIsCodingTest/ch06_sort/No_06.java
@@ -0,0 +1,32 @@
+package thisIsCodingTest.ch06_sort;
+
+public class No_06 {
+    public static void main(String args[]) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2};
+        int[] count = new int[10];
+
+        /*
+        // 카운트 배열 0으로 초기화
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+        }
+        */
+
+        // 각 데이터에 해당하는 인덱스 값 증가
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+
+        // 각 인덱스 개수만큼 출력
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                sb.append(i + " ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
