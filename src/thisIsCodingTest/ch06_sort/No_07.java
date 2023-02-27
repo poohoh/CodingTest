33b2080ba61ab619cc64784a93734ddfac21366b
poohoh <kjh980328@gmail.com>
2023-02-27 11:34:48 +0900

    ThisIsCodingTest ch06_sort No_07 정렬 라이브러리 기본 예제

---
+++ src/thisIsCodingTest/ch06_sort/No_07.java
@@ -0,0 +1,15 @@
+package thisIsCodingTest.ch06_sort;
+
+import java.util.Arrays;
+
+public class No_07 {
+    public static void main(String args[]) {
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8,};
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
