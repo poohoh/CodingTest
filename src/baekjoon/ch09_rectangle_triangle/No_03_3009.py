cd2fcd3527af3d03a028f01d9a3a486cd6d1d794
poohoh <kjh980328@gmail.com>
2023-03-27 20:10:58 +0900

    Baekjoon ch09 No_03_3009.py

---
+++ src/baekjoon/ch09_rectangle_triangle/No_03_3009.py
@@ -0,0 +1,15 @@
+x_nums = []
+y_nums = []
+
+for _ in range(3):
+    x, y = map(int, input().split())
+    x_nums.append(x)
+    y_nums.append(y)
+
+for i in range(3):
+    if x_nums.count(x_nums[i]) == 1:
+        x = x_nums[i]
+    if y_nums.count(y_nums[i]) == 1:
+        y = y_nums[i]
+
+print(x, y)
