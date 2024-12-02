676502ba9083a35d9e2a699b435e3a48cd80dbd3
poohoh <kjh980328@gmail.com>
2024-12-02 22:21:55 +0900

    Baekjoon class3 - 2805_2, timeout

---
+++ src/baekjoon/class_03/2805_2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start = 0
+end = max(trees)
+height = end // 2
+
+while True:
+    # sum of cut over height is larger than M
+    if sum([max(t-height, 0) for t in trees]) >= M:
+        # sum of cut over (height+1) is smaller than M
+        if sum([max(t-(height+1), 0) for t in trees]) < M:
+            break
+        else:
+            start = height
+            height = (end+height) // 2
+    else:
+        end = height
+        height = (start+height) // 2
+
+print(height)
